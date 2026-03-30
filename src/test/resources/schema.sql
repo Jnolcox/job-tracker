b735dd0c2e5fe75e3c450422d76d46761de4b641
John Nolcox <Jnolcox0429+github@gmail.com>
2026-03-29 23:38:13 -0700

    added role level to schema

--- src/test/resources/schema.sql
+++ src/test/resources/schema.sql
@@ -25,6 +25,7 @@ CREATE TABLE IF NOT EXISTS job_applications (
     salary_max DOUBLE,
     location VARCHAR(255),
     rto_type VARCHAR(50),
+    level VARCHAR(50),
     contact_name VARCHAR(255),
     contact_email VARCHAR(255),
     contact_phone VARCHAR(255),
